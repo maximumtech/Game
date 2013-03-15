b4cdb8cd2f841c6704d22fd4cce6cc68b9247902
Max Bruce <maximumtech11@gmail.com>
2013-03-14 20:46:15 -0700

    switched to netbeans

--- Game/src/Game/base/GameBase.java
+++ Game/src/Game/base/GameBase.java
@@ -1,9 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Game.base;
 
+/**
+ *
+ * @author Max
+ */
 public class GameBase {
-	
-	public static void main(String[] args) {
-		
-	}
-	
+    
+    public static void main(String[] args) {
+        
+    }
+    
 }
