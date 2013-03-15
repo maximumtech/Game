8a10296bb8cf498044d5784bfcbc44e66371bcf5
Max Bruce <maximumtech11@gmail.com>
2013-03-14 21:39:24 -0700

    added javadocs & touchups

--- Game/src/Game/base/GameBase.java
+++ Game/src/Game/base/GameBase.java
@@ -1,8 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Game.base;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import java.util.Random;
 
 /**
  *
@@ -10,8 +12,64 @@ package Game.base;
  */
 public class GameBase {
     
+    public static GameBase instance;
+    public Random rand;
+    
     public static void main(String[] args) {
+        new GameBase(args);
+    }
+
+    public GameBase(String[] args) {
+        try {
+            Display.setDisplayMode(new DisplayMode(800, 600));
+            Display.setTitle("Game");
+            Display.setResizable(true);
+            Display.create();
+            System.out.println("Display Created!");
+        } catch (LWJGLException e) {
+            System.out.println("Failed to create display.");
+            e.printStackTrace();
+        }
+        System.out.println("Starting OpenGL");
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0, 800, 600, 0, 1, -1);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        System.out.println("Initialized");
+        System.out.println("Starting Render Loop");
+        instance = this;
+        while (!closeRequested()) {
+            if (Display.wasResized()) {
+                GL11.glViewport(0, 0, Display.getWidth(), Display.getHeight());
+            }
+            render();
+            Display.update();
+            Display.sync(30);
+        }
+        System.out.println("Closing");
+        Display.destroy();
+    }
+    
+    private void render() {
         
     }
     
+    private static boolean closing = false;
+    
+    public static void setClosing() {
+        closing = true;
+    }
+    
+    public static boolean closeRequested() {
+        try {
+            boolean temp = Display.isCloseRequested() || closing;
+            return temp;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return closing;
+    }
 }
