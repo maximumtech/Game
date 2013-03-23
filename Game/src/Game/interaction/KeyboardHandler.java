d2e98657799a2499bb74bbe1d36ade2536859505
Max Bruce <maximumtech11@gmail.com>
2013-03-23 14:03:45 -0700

    fix exit crash

--- Game/src/Game/interaction/KeyboardHandler.java
+++ Game/src/Game/interaction/KeyboardHandler.java
@@ -8,7 +8,7 @@ import org.lwjgl.input.Keyboard;
  * @author maximumtech
  */
 public class KeyboardHandler {
-    
+
     public static KeyboardHandler instance = new KeyboardHandler();
     private boolean[] states = new boolean[256];
     private long[] millis = new long[states.length];
@@ -17,47 +17,48 @@ public class KeyboardHandler {
     public void registerHandler(IKeyboardHandler handler) {
         handlers.add(handler);
     }
-    
+
     public KeyboardHandler() {
         for (int i = 0; i < states.length; i++) {
             states[i] = false;
             millis[i] = 0L;
         }
     }
-    
+
     public void onUpdate() {
-        for (int i = 0; i < states.length; i++) {
-            boolean wasDown = states[i];
-            boolean isDown = Keyboard.isKeyDown(i);
-            if(!wasDown && isDown) {
-                keyDown(Keyboard.getKeyName(i).toLowerCase());
-                states[i] = true;
-                millis[i] = System.currentTimeMillis();
-            }else if(wasDown && !isDown) {
-                keyUp(Keyboard.getKeyName(i).toLowerCase(), System.currentTimeMillis() - millis[i]);
-                states[i] = false;
-            }else if(isDown){
-                keyHeld(Keyboard.getKeyName(i).toLowerCase(), System.currentTimeMillis() - millis[i]);
+        if (Keyboard.isCreated()) {
+            for (int i = 0; i < states.length; i++) {
+                boolean wasDown = states[i];
+                boolean isDown = Keyboard.isKeyDown(i);
+                if (!wasDown && isDown) {
+                    keyDown(Keyboard.getKeyName(i).toLowerCase());
+                    states[i] = true;
+                    millis[i] = System.currentTimeMillis();
+                } else if (wasDown && !isDown) {
+                    keyUp(Keyboard.getKeyName(i).toLowerCase(), System.currentTimeMillis() - millis[i]);
+                    states[i] = false;
+                } else if (isDown) {
+                    keyHeld(Keyboard.getKeyName(i).toLowerCase(), System.currentTimeMillis() - millis[i]);
+                }
             }
         }
     }
-    
+
     public void keyDown(String key) {
-        for(IKeyboardHandler handler : handlers) {
+        for (IKeyboardHandler handler : handlers) {
             handler.keyDown(key);
         }
     }
-    
+
     public void keyHeld(String key, long ms) {
         for (IKeyboardHandler handler : handlers) {
             handler.keyHeld(key, ms);
         }
     }
-    
+
     public void keyUp(String key, long ms) {
         for (IKeyboardHandler handler : handlers) {
             handler.keyUp(key, ms);
         }
     }
-    
 }
